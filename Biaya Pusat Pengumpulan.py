b0014313387995964a89c52bc81bcf7b0eaf3ee6
faiqul12 <102243861+faiqul12@users.noreply.github.com>
2024-08-30 14:08:31 +0700

    first commit

---
+++ Biaya Pusat Pengumpulan.py
@@ -0,0 +1,17 @@
+''' 
+# Biaya Pendirian Fasilitas Pengumpulan
+    fcl = Biaya tetap untuk mendirikan pusat pengumpulan (l) ($)
+    ul = variabel biner bernilai 1 jika pusat pengumpulan (l) didirikan dan bernilai 0 jika sebaliknya. 
+
+# Biaya Pengiriman ke Pemanufaktur
+    Cdv =  Biaya transportasi untuk pengiriman satu unit produk atau produk bekas pakai atau komponen dari produk bekas pakai dengan menggunakan 
+            kendaraan (v) ($/unit)Cdv	Biaya transportasi untuk pengiriman satu unit produk atau produk bekas pakai atau komponen dari produk bekas 
+            pakai dengan menggunakan kendaraan (v) ($/unit)
+    dli = Jarak dari Penampungan ke Pemanufaktur
+    Cri  = Kapasitas remanufaktur (unit) == jumlah yg dikirim ke remanufaktur (?)
+    dli = Jarak dari Penampungan ke Pemanufaktur
+
+# Biaya Pengiriman ke Pembuangan
+    
+    dlm = Jarak dari Penampungan ke Pembuangan
+'''
