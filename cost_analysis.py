e11461d1d5b041dbafaa76270552fb57d35532e4
NanoCipto <nanociptoo@gmail.com>
2024-09-01 11:48:45 +0700

    tambah keterangan

--- cost_analysis.py
+++ cost_analysis.py
@@ -3,23 +3,24 @@ import pulp as lp
 #DECLARATION VARIABLES
 
 #Stakeholder
-I = 1 # Pemanufaktur
+I = 2 # Pemanufaktur
 J = 2 # Distributor
 K = 4 # Konsumen
-M = 2 # Pengumpulan
-N = 2 # Pembuangan
+L = 2 # Pengumpulan
+M = 2 # Pembuangan
 
 # BIAYA PEMANUFAKTUR
 
 ## PRODUCTION COST
 # 1. Biaya
-production_cost = [12.5, 15.0, 17.5, 19.5] # -> Asumsi Cpi = PCi
+# production cost = Cpi/PCi
+Cpi = [12.5, 15.0, 17.5, 19.5] # -> Asumsi Cpi = PCi
 
 # 2. Jumlah yang diproduksi dan dikirimkan
 production_delivery_quantity = [100000, 110000, 120000, 130000] #Qpij = Dk -> Jumlah yang diproduksi = yang Dikirim = Permintaan 
 
 ### PERHITUNGAN PRODUCTION COST
-perhitungan_production_cost = [production_cost[i]*production_delivery_quantity[i] for i in range(4)]
+perhitungan_production_cost = [Cpi[i]*production_delivery_quantity[i] for i in range(4)]
 print(perhitungan_production_cost)
 
 ## REMANUFACTURE COST
@@ -46,8 +47,8 @@ cost_vehicle_per_distance = {
 # 2. Jarak 
 distance_i_j = [150, 200, 300, 500]
 distance_j_k = [100, 150, 200, 350]
-distance_k_m = [90, 95, 97, 100]
-distance_m_i = [200, 300, 400, 650]
+distance_k_l = [90, 95, 97, 100]
+distance_l_m = [200, 300, 400, 650]
 distance_m_n = [250, 500, 600, 750]
 
 # 3. Jumlah Produk Yang Dikirimkan Dari Pemanufaktur ke Distributor 
@@ -95,8 +96,92 @@ problem += (
     lp.lpSum(biaya_diskon[d] for d in biaya_produksi) 
 )
 
-#Fungsi Batasan
-# ... Menyesuaikan
+'''
+I = PEMANUFAKTUR (i)
+J = DISTRIBUTOR (j)
+K = KONSUMEN (k)
+L = PENGUMPULAN (l)
+M = PEMBUANGAN (m)
+'''
+
+
+#CONSTRAINT MODEL
+# 1. QPij >= Dk  
+# Jumlah Produk yang Diproduksi Pemanufaktur >= Demand Produk
+
+
+
+# 2. QDjk <= QPij
+# Jumlah Produk yang Dikirim ke Distributor <= Jumlah Jumlah Produk yang Diproduksi Pemanufaktur
+
+
+
+# 3. QRli <= (1 - Fd) (Rkl + ... + n)
+# Jumlah Produk Penampungan ke Pemanufaktur <= Rumus x Jumlah Produk Konsumen ke Penampungan
+
+
+
+# 4. QSlm == Fd (Rkl + ... + n)
+# Jumlah Produk Pengumpulan ke Pembuangan == Rumus x Jumlah Produk Konsumen ke Penampungan
+
+
+
+# 5. Rkl <= yk (QDjk x GLti + ... + n)
+# Jumlah Produk Konsumen ke Penampungan <= yk x Jumlah (Produk Dikirim ke Distributor x Biner 0,1 -> nongreen/green)
+
+
+
+# 6. QRli <= M (QPij + ... + n)
+# Jumlah Penampungan ke Pemanufaktur <= M (?) x (Jumlah Pemanufaktur ke Distributor)
+
+
+
+# 7. QMi <= Cppi
+# Jumlah Produk yang Diproduksi Pemanufaktur <= Kapasitas Produksi
+
+
+
+# 8. QDjk <= Cpdj
+# Jumlah Produk Dikirim Distributor ke Konsumen <= Kapasitas Distributor
+
+
+
+# 9. Rkl <= ul Cccl
+# Jumlah Produk Dari Konsumen ke Distributor <= ul (?) x Kapasitas Pengumpulan
+
+
+
+# 10. QRli <= Cri
+# Jumlah Produk Pengumpulan ke Pemanufaktur <= Kapasitas Remanufaktur
+
+
+
+# 11. QSlm <= vm Ccdm
+# Jumlah Produk Dibuang <= vm (?) x Kapasitas Pembuangan
+
+
+
+# 12. yk = (Pd/Png x δk/δ* be^-φq)
+# yk (?) = (?)
+
+
+
+# 13. Cpi = PCi + βi (t GLti + ... + n)
+# Biaya Produksi = Biaya Produksi Produk non Green x Green Factor 
+
+
+
+# 14. Pd <= Pngi 
+# (?)
+
+
+
+# 15. QMi, Qrli, QSlm, Qpij, Qdjk, Rkl Pd, Cpi >= 0
+# Semua Jenis Kuantitas dan Kapasitas Produk >= 0
+
+
+# 16. ul, vm, GLti ∈ {0,1}
+# Faktor merupakan Elemen Bilangan Biner (0 atau 1)
 
 #Penyelesaian
 problem.writeLP("Cost_Minimization")
@@ -121,7 +206,7 @@ print("Total Biaya =", lp.value(problem.objective))
 
 #LIST PERTANYAAN
 '''
-1. APAKAH Cpi = PCi?
+1. APAKAH Cpi = PCi? -> Ada Rumusnya Di Constraint
 2. APAKAH KUANTITAS YANG DIPRODUKSI = KAPASITAS PRODUKSI?
 3.
 '''
