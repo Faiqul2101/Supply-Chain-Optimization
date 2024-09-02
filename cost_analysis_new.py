2f77b605d15229d3883e5386f3e30714e0404bd8
nanocipto <hafizh.maulana.ub@gmail.com>
2024-09-02 15:57:46 +0700

    baru nambah data sama variable

--- cost_analysis_new.py
+++ cost_analysis_new.py
@@ -1,222 +1,207 @@
 import pulp as lp
 
 # PARAMETER
-Manufaktur = ["M1", "M2"]
-Distributor = ["D1", "D2"]
-Konsumen = ["K1", "K2", "K3", "K4"]
-Pengumpulan = ["P1", "P2"]
-Pembuangan = ["PB1", "PB2"]
 
-## DATA COLLECTION
-# Kuantitas dan Kapasitas
-Dk =  [100000, 130000]  # --> Demand Produk 
-Cppi = [135000, 150000] # --> Kapasitas Maksimal Produksi Manufaktur
-Cri = [88000, 92000]      #--> Kapasitas Maksimal Remanufaktur
-CPdj = [50000, 60000]     #--> Kapasitas Maksimal Distributor
-Cccl = [70000, 95000]     #--> Kapasitas Maksimal Pusat Pengumpulan
-Ccdm = [80000, 100000]   #--> Kapasitas Maksimal Pusat Pembuangan
+Cdv = { # Asumsi Trailer Petrol dan Truck Petrol
+    "ij" : {
+        "i1 ke j1" : 4,
+        "i1 ke j2" : 5,
+        "i2 ke j1" : 4,
+        "i2 ke j2" : 4,
+    },
+    
+    "jk" : {
+        "j1 ke K1" : 9,
+        "j1 ke K2" : 10,
+        "j1 ke K3" : 9,
+        "j1 ke K4" : 10,
+        "j2 ke K1" : 9,
+        "j2 ke K2" : 10,
+        "j2 ke K3" : 9,
+        "j2 ke K4" : 10,
+    },
 
-# Fixed Cost
-fcl = [400000, 500000]  #--> Biaya Pendirian Fasilitas Pengumpulan
-fbm = [300000, 350000]  #--> Biaya Pendirian Fasilitas Pembuangan
+    "li" : {
+        "l1 ke i1" : 9,
+        "l1 ke i2" : 10,
+        "l2 ke i1" : 9,
+        "l2 ke i2" : 10,
+    },
 
-# Variable Cost
-Coj = [5, 8]              #--> Biaya Penanganan Produk di Distributor 
-Ccl = [13, 17]          #--> Biaya Pemilahan Produk di Pengumpulan
-Crei = [8, 13]           #--> Biaya Remanufaktur di Pemanufaktur
-Cdl = [10, 14]          #--> Biaya Pemusnahan di Pembuangan
-
-PCi = [12.5, 19.5]  #--> Biaya Produksi Non Hijau
-
-# Delivery Cost (Vehicle Based)
-Cdv = {
-    "Truck (Hybrid Motor)": [13, 14],
-    "Truck with Petrol" : [9, 10],
-    "Trailer (Hybrid Motor)": [6, 7],
-    "Trailer with Petrol" : [4, 5]
+    "lm" : {
+        "l1 ke m1" : 9,
+        "l1 ke m2" : 10,
+        "l2 ke m1" : 9,
+        "l2 ke m2" : 10,
+    }
 }
 
-# Cdv_Temporary = [13, 9, 6, 4]
+d = {
+    "ij" : {
+        "i1 ke j1" : 150,
+        "i1 ke j2" : 500,
+        "i2 ke j1" : 150,
+        "i2 ke j2" : 500,
+    },
+    
+    "jk" : {
+        "j1 ke K1" : 100,
+        "j1 ke K2" : 350,
+        "j1 ke K3" : 100,
+        "j1 ke K4" : 350,
+        "j2 ke K1" : 100,
+        "j2 ke K2" : 350,
+        "j2 ke K3" : 100,
+        "j2 ke K4" : 350,
+    },
 
-Cdv_keys = Cdv.keys()
-Cdv_dicts = lp.LpVariable.dicts("Vehicles",Cdv_keys,lowBound=0,upBound=None,cat=lp.LpContinuous)
+    "kl" : {
+        "k1 ke l1" : 90,
+        "k1 ke l2" : 100,
+        "k2 ke l1" : 90,
+        "k2 ke l2" : 100,
+        "k3 ke l1" : 90,
+        "k3 ke l2" : 100,
+        "k4 ke l1" : 90,
+        "k4 ke l2" : 100,
+    },
 
-print(Cdv_dicts)
-# Distance
-dij = [150, 200, 300, 500]  #--> Distance Pemanufaktur (i) ke Distributor (j)
-djk = [100, 150, 200, 350]  #--> Distance Distributor (j) ke Konsumen (k)
-dkl = [90, 95, 97, 100]     #--> Distance Konsumen (k) ke Pengumpulan (l)
-dli = [200, 300, 400, 650]  #--> Distance Pengumpulan (l) ke Pemanufaktur (i)
-dlm = [250, 500, 600, 750]  #--> Distance Pengumpulan (l) ke Pembuangan (m)
+    "li" : {
+        "l1 ke i1" : 200,
+        "l1 ke i2" : 650,
+        "l2 ke i1" : 200,
+        "l2 ke i2" : 650,
+    },
 
-# Faktor-Faktor Biner
-GLti = [0, 1, 0, 1]         #--> Produksi Green Produk atau Tidak (1,0)
-ul = [0, 1, 0, 1]           #--> Fasilitas Pengumpulan Dibangun atau Tidak (1,0)
-vm = [0, 1, 0, 1]           #--> Fasilitas Pembuangan Dibangun atau Tidak (1,0)
+    "lm" : {
+        "l1 ke m1" : 250,
+        "l1 ke m2" : 750,
+        "l2 ke m1" : 250,
+        "l2 ke m2" : 750,
+    }
+}
 
-# Faktor-Faktor Pendukung Lain
-B = [0.1, 0.3]              #--> Faktor Biaya Produksi Green
-Pngi = [50, 55]     #--> Harga Produk Non-Green
-Png = [55, 55]      #--> Maksimum Harga Produk Non-Green
-Pd = [0, 55]        #--> Maksimum Penawaran Diskon
-Fd = [0.5, 0.5]   #--> Proporsi Produk Bekas Pakai Yang Dibuang
-q = [0.3, 0.5]   #--> Tingkat Penerimaan Kualitas Produk Bekas Pakai (Menghitung Yk)
-a = [0, 10]            #--> Tingkat Kesadaran Lingkungan Konsumen Berdasarkan Zona
+# MANUFACTURE COST
 
-### PEMANUFAKTUR
-## PRODUCTION COST (Cpi x QPij)
-Cpi = [PCi[item] + B[item] * GLti[item] for item in range(4)]   #--> Cpi = PCi x Faktor Green
-QPij = [Dk[item] * 0.8 for item in range(4)]                    #QPij = Dk x Proporsi
-production_cost = [Cpi[i]*QPij[i] for i in range(4)]
-print(production_cost)
+PCi = {
+    "Manufaktur 1" : 12.5,
+    "Manufaktur 2" : 19.5
+}
+manuf_keys = PCi.keys()
+Cre = {
+    "Manufaktur 1" : 8,
+    "Manufaktur 2" : 13
+}
 
-## DELIVERY COST MANUFACTURE -> DISTRIBUTOR (Cdv x dij x QPij) --> Cdv Belom
-# delivery_cost_ij = [Cdv_dicts[item] *dij[i]*QPij[i] for item in Cdv for i in range(4)]
-delivery_cost_ij = [Cdv_Temporary[i] *dij[i]*QPij[i] for i in range(4)]
+Beta = {
+    "Manufaktur 1" : 0.1,
+    "Manufaktur 2" : 0.3
+}
 
-### DISTRIBUTOR
-## HANDLING COST (BIAYA PENANGANAN) (Coj x QPij)
-handling_cost = [Coj[i]*QPij[i] for i in range(4)]
+Png = {
+    "Manufaktur 1" : 50,
+    "Manufaktur 2" : 55
+}
 
-## DELIVERY COST DISTRIBUTOR -> KONSUMEN (Cdv x djk x QDjk)
-QDjk = [QPij[item] * 0.8 for item in range(4)]
-delivery_cost_jk = [Cdv_Temporary[i]* djk[i]*QDjk[i] for i in range(4)]
+q = {
+    "Manufaktur 1" : 0.3,
+    "Manufaktur 2" : 0.5
+}
 
-### PENGUMPULAN
-## BUILD FACILITY COST L (fcl x ul)
-facility_cost_l = [fcl[i] * ul[i] for i in range(4)]
+b = {
+    "Manufaktur 1" : 0.1,
+    "Manufaktur 2" : 0.3
+}
 
-## SORTING COST (Cccl x Rkl)
-Yk = [Pd[i]/Png[i] * a[i]/3 for i in range(4)]                  #--> Tingkat Pengembalian Produk Bekas Pakai
-# Rkl = [QDjk[item] * 0.8 for item in range(4)]                 #--> Ada Rumusnya Rkl <= Yk x QDjk x GLti
-Rkl = [QDjk[item] * Yk[item] * GLti[item] for item in range(4)] #--> Jumlah Produk Bekas Pakai yang Dikembalikan Konsumen      
-sorting_cost = [Cccl[i] * Rkl[i] for i in range(4)]
+dd = {
+    "Manufaktur 1" : 0.3,
+    "Manufaktur 2" : 0.5
+}
+# DISTRIBUTOR COST
 
-## DELIVERY COST PENGUMPULAN -> PEMANUFAKTUR (Cdv x dli x QRli)
-QRli = [Rkl[item] * 0.5 for item in range(4)]
-delivery_cost_li = [Cdv_Temporary[i]*dli[i]*QRli[i] for i in range(4)]
+Coj = {
+    "Distributor 1" : 5,
+    "Distributor 2" : 8
+}
 
-## DELIVERY COST PENGUMPULAN -> PEMBUANGAN (Cdv x dlm x QSlm)
-QSlm = [Rkl[item] * 0.5 for item in range(4)]
-delivery_cost_lm = [Cdv_Temporary[i]*dlm[i]*QSlm[i] for i in range(4)]
+# COLLECTOR COST
 
-### PEMANUFAKTUR (2)
-## REMANUFACTURE COST (Crei x QRli)
-remanufacture_cost = [Crei[i]*QRli[i] for i in range(4)]
+Ccl = {
+    "Collector 1" : 13,
+    "Collector 2" : 17
+}
 
-## DISCOUNT COST (QRli x Pd x Dd)
-Dd = [0.5, 0.6, 0.7, 0.8] # Asumsi 
-### PERHITUNGAN DISCOUNT COST
-discount_cost = [Pd[i]*QRli[i]*Dd[i] for i in range(4)]
+Fcl = {
+    "Collector 1" : 20*20000,
+    "Collector 2" : 20*25000
+}
 
-### PEMBUANGAN
-## BUILD FACILITY COST (2) (fbm x vm)
-facility_cost_m = [fbm[i] * vm[i] for i in range(4)]
+# DISPOSER COST
 
-## DISPOSAL COST (Cdl x QSlm)
-disposal_cost = [Cdl[i] * QSlm[i] for i in range (4)]
+Cdm = {
+    "Disposer 1" : 10,
+    "Disposer 2" : 13
+}
 
-#MODEL
-problem = lp.LpProblem("Cost_Minimization", lp.LpMinimize)
+Fbm = {
+    "Disposer 1" : 10*30000,
+    "Disposer 2" : 10*35000
+}
 
-totalcost = []
+# CAPACITY 
+Cppi ={
+    "Manufaktur 1" : 135000,
+    "Manufaktur 2" : 150000
+}
 
-#OBJECTIVE FUNCTION TOTAL COST
-problem += lp.lpSum(totalcost),"Total Cost :"
+Cri = {
+    "Manufaktur 1" : 88000,
+    "Manufaktur 2" : 92000
+}
 
-'''
-I = PEMANUFAKTUR (i)
-J = DISTRIBUTOR (j)
-K = KONSUMEN (k)
-L = PENGUMPULAN (l)
-M = PEMBUANGAN (m)
-'''
+Cpdj = {
+    "Distributor 1" : 50000,
+    "Distributor 2" : 60000   
+}
 
-#CONSTRAINT MODEL
-# 1. QPij >= Dk  
-# Jumlah Produk yang Diproduksi Pemanufaktur (Dikirim ke Distributor) >= Demand Produk
-problem += [QPij[i] for i in range(4)] >= [Dk[i] for i in range(4)]
+Cccl = {
+    "Collector 1" : 70000,
+    "Collector 2" : 95000
+}
 
-# 2. QDjk <= QPij
-# Jumlah Produk yang Dikirim ke Distributor <= Jumlah Jumlah Produk yang Diproduksi Pemanufaktur
-problem += [QDjk[i] for i in range(4)] <= [QPij[i] for i in range(4)]
+Ccdm = {
+    "Disposer 1" : 80000,
+    "Disposer 2" : 100000
+}
 
 
-# 3. QRli <= (1 - Fd) (Rkl + ... + n)
-# Jumlah Produk Penampungan ke Pemanufaktur <= Rumus x Jumlah Produk Konsumen ke Penampungan
-problem += [QRli[i] for i in range(4)] <= [(1-Fd[i]) * Rkl[i] for i in range(4)]
+# DEMAND
+Dk = {
+    "Konsumen 1" : 100000,
+    "Konsumen 2" : 110000,
+    "Konsumen 3" : 120000,
+    "Konsumen 4" : 130000
+}
 
-# 4. QSlm == Fd (Rkl + ... + n)
-# Jumlah Produk Pengumpulan ke Pembuangan == Rumus x Jumlah Produk Konsumen ke Penampungan
-problem += [QSlm[i] for i in range(4)] == [Fd[i] * Rkl[i] for i in range(4)]
+konsumen_keys = Dk.keys()
+collector_keys = Cccl.keys()
+disposer_keys = Ccdm.keys()
+problem = lp.LpProblem("Supply Chain Optimization", lp.LpMinimize)\
 
-# 5. Rkl <= yk (QDjk x GLti + ... + n)
-# Jumlah Produk Konsumen ke Penampungan <= yk x Jumlah (Produk Dikirim ke Distributor x Biner 0,1 -> nongreen/green)
-problem += [Rkl[i] for i in range(4)] <= [QDjk[item] * Yk[item] * GLti[item] for item in range(4)]
+# Variabel Manufaktur
+Qpij = lp.LpVariable.dicts("jumlah_produksi_manuf", manuf_keys, 0,None,cat=lp.LpInteger)
+Pd = lp.LpVariable.dicts("penawaran_diskon", manuf_keys, 0,None,cat=lp.LpInteger)
+Qrli = lp.LpVariable.dicts("jumlah_produk_remanufaktur", manuf_keys, 0,None,cat=lp.LpInteger)
+GLti = lp.LpVariable.dicts("parameter_ramah", manuf_keys, 0,1,cat=lp.LpBinary)
+# Variabel Distributor
+Qdjk = lp.LpVariable.dicts("produk_to_konsum",konsumen_keys,lowBound=0,upBound=None,cat=lp.LpInteger)
 
-# 6. QRli <= M (QPij + ... + n)
-# Jumlah Penampungan ke Pemanufaktur <= M (?) x (Jumlah Pemanufaktur ke Distributor)
-problem += [QRli[i] for i in range(4)] <= [QPij[i] for i in range(4)]
+# Variabel Konsumen
+Rkl =  lp.LpVariable.dicts("konsumen_to_collector",collector_keys,lowBound=0,upBound=None,cat=lp.LpInteger)
 
-# 7. QMi atau QPij <= Cppi
-# Jumlah Produk yang Diproduksi Pemanufaktur <= Kapasitas Produksi
-problem += [QPij[i] for i in range(4)] <= [Cppi[i] for i in range(4)]
+# Variabel Collector
+Ul = lp.LpVariable.dicts("parameter_bangun_collector", collector_keys, 0,1,cat=lp.LpBinary)
 
-# 8. QDjk <= Cpdj
-# Jumlah Produk Dikirim Distributor ke Konsumen <= Kapasitas Distributor
-problem += [QDjk[i] for i in range(4)] <= [CPdj[i] for i in range(4)]
-
-# 9. Rkl <= ul Cccl
-# Jumlah Produk Dari Konsumen ke Distributor <= ul (?) x Kapasitas Pengumpulan
-problem += [Rkl[i] for i in range(4)] <= [ul[i] * Cccl[i] for i in range(4)]
-
-# 10. QRli <= Cri
-# Jumlah Produk Pengumpulan ke Pemanufaktur <= Kapasitas Remanufaktur
-problem += [QRli[i] for i in range(4)] <= [Cri[i] for i in range(4)]
-
-# 11. QSlm <= vm Ccdm
-# Jumlah Produk Dibuang <= vm (?) x Kapasitas Pembuangan
-problem += [QSlm[i] for i in range(4)] <= [vm[i] * Ccdm[i] for i in range(4)]
-
-# 12. yk = (Pd/Png x δk/δ* be^-φq) --> Sudah Di Rumus Perhitungan Yk
-
-# 13. Cpi = PCi + βi (t GLti + ... + n) --> Sudah Di Perhitungan Biaya Produksi
-
-# 14. Pd <= Pngi 
-problem += [Pd[i] for i in range(4)] <= [Pngi[i] for i in range(4)]
-
-# 15. QMi, Qrli, QSlm, Qpij, Qdjk, Rkl Pd, Cpi >= 0 --> Otomatis Dari Perhitungan Dan Variabel
-
-# 16. ul, vm, GLti ∈ {0,1} --> Otomatis Dari Perhitungan Dan Variabel
-
-#Penyelesaian
-problem.writeLP("Cost_Minimization")
-problem.solve()
-print("Status:", lp.LpStatus[problem.status])
-
-print(problem)
-status = problem.solve()
-print(lp.LpStatus[status])
-
-for v in problem.variables():
-    print(v.name, "=", v.varValue)
-
-print("Total Biaya =", lp.value(problem.objective))
-
-# NOTES
-'''
-1. KAPASITAS DIGUNAKAN SEBAGAI FUNGSI BATASAN
-2.
-'''
-
-#LIST PERTANYAAN
-'''
-1. Rumus be^-φq serta masing-masing variable itu apa?
-2. Proporsi Untuk Masing-Masing Perpindahan itu Berapa?
-3. Jumlah Tempat Manufaktur, Distributor, Konsumen dst itu kan berbagai macam.
-   Sedangkan Datanya berbentuk Distribusi, Contohnya Jarak. Nah itu nanti untuk Jumlah Tempatnya
-   akan Berpengaruh ke perhitungan atau mengikuti distribusi saja?
-4. GLti perhitungan faktor binernya seperti apa? Apakah 1 objek diasumsikan memiliki satu faktor biner
-   (A : 1, B : 0 dst) atau satu faktor disimulasikan 2 kali? (A:0, A:1, B:0, B:2)
-5. Dd (Penawaran Diskon) datanya didapat darimana?
-'''
+# Variabel Disposer
+Vm = lp.LpVariable.dicts("parameter_bangun_disposer", disposer_keys, 0,1,cat=lp.LpBinary)
+Qslm =  lp.LpVariable.dicts("produk_dibuang",manuf_keys,lowBound=0,upBound=None,cat=lp.LpInteger)
